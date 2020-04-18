52d770d822ec000f189fea4983c83e4e8e1e1cf3
Vicky <vicky@Vickys-MBP.default>
2020-04-18 13:26:26 +0100

    building the structure of the app

---
+++ src/Todos/Todos.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class Todos extends Component {
+  //initialize the statement of the list
+  constructor() {
+    super();
+  }
+
+  // addItem function
+
+  //deleteItem function
+
+  render() {
+
+
+    
+    return (
+    <>
+    </>
+    
+    
+    );
+  }
+}
