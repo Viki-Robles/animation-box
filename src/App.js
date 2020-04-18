1429de01b2ef8e07609561a4f9df9034d1148906
Vicky <vicky@Vickys-MBP.default>
2020-04-18 17:42:24 +0100

    building the handleChange function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Todos from './Todos/Todos';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>todos</h1>
+      <Todos/>
+      <Footer/>
     </div>
   );
 }
--- src/Todos/Todos.js
+++ src/Todos/Todos.js
@@ -1,24 +1,45 @@
 import React, { Component } from "react";
+import "./Todos.css";
+
+// STEPS FOR TODO
+
+//add Todo
+// display Todo
+// cross off todo
+//show number of active todos
+//filter all/active/complete
+//delete todo
+//delete all complete
+//button to toggle all on/off
 
 export default class Todos extends Component {
-  //initialize the statement of the list
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = { items: "" };
   }
 
-  // addItem function
+  //1. HanldeChange function
 
-  //deleteItem function
+  handleChange = (event)=> {
+    this.setState({
+      [event.target.name]:[event.target.value]
+    });
+  }
 
   render() {
-
-
-    
     return (
-    <>
-    </>
-    
-    
+      <>
+        <form>
+          <input
+            name="items"
+            type="text"
+            placeholder="What needs to be done?"
+            value={this.state.items}
+            onChange={this.handleChange}
+          ></input>
+        </form>
+        <button type="submit">Add</button>
+      </>
     );
   }
 }
